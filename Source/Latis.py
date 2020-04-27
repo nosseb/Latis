e33519513c5f4494b7ffedd22cbe587085bb7c3b
Robinson <robinson.besson@nosseb.fr>
2020-04-27 21:59:23 +0200

    Typo

--- Source/Latis.py
+++ Source/Latis.py
@@ -1,15 +1,16 @@
-import io           # Nécessaire car le fichier est encodé en 'utf-16-le'
-import xmltodict    # Pour lire facillement le fichier xml
+import io  # Nécessaire car le fichier est encodé en 'utf-16-le'
+import xmltodict  # Pour lire facilement le fichier xml
 import struct
 import binascii
 
-def netoyage(texte :str):
-    # Le fihier ne respecte pas totalement la norme xml
+
+def nettoyage(texte: str):
+    # Le fishier ne respecte pas totalement la norme xml
     # La première partie (qui nous intéresse) est valide
     # On va donc l'extraire
 
     pos = texte.find("</BLOC-COURBES>")
-    return texte[0:pos+15]
+    return texte[0:pos + 15]
 
 
 def decoupe(table):
@@ -17,19 +18,19 @@ def decoupe(table):
     pass
 
 
-def convertion(table):
-    # Renvoie une vouvelle table avec des float au lieux de str
+def conversion(table):
+    # Renvoie une nouvelle table avec des float au lieu de str
     # struct.unpack('d', binascii.unhexify("3F5205BC01A36E2F"))
     pass
 
 
-def extract(path: str) :
-    # En théorie retourne une liste de listes de liste de valeurs:
+def extract(path: str):
+    # En théorie retourne une liste de listes de liste de valeurs :
     # Chaque courbe latis pro contient deux listes de valeurs, une pour x et une pour y
     # Il y a potentiellement plusieurs courbes dans le fichier
-    
-    with io.open(path, 'r', encoding='utf-16-le') as file:    # Chargement du fichier
-        content = netoyage(file.read())
+
+    with io.open(path, 'r', encoding='utf-16-le') as file:  # Chargement du fichier
+        content = nettoyage(file.read())
 
         doc = xmltodict.parse(content)
 
@@ -37,12 +38,13 @@ def extract(path: str) :
 
         table_string = []
 
-        # Récupération des chaines exadécimales
-        for i in range(nb_courbes) :
+        # Récupération des chaines hexadécimales
+        for i in range(nb_courbes):
             table_string.append([])
-            if doc["BLOC-COURBES"]["LESCOURBES"]["C"]["C" + str(i)]["DATAX"]["DonneesX"]["DONNEES"] :
-                table_string[i].append(doc["BLOC-COURBES"]["LESCOURBES"]["C"]["C" + str(i)]["DATAX"]["DonneesX"]["DONNEES"])
-            table_string[i].append( doc["BLOC-COURBES"]["LESCOURBES"]["C"]["C" + str(i)]["DATAY"]["DonneesY"]["DONNEES"] )
+            if doc["BLOC-COURBES"]["LESCOURBES"]["C"]["C" + str(i)]["DATAX"]["DonneesX"]["DONNEES"]:
+                table_string[i].append(
+                    doc["BLOC-COURBES"]["LESCOURBES"]["C"]["C" + str(i)]["DATAX"]["DonneesX"]["DONNEES"])
+            table_string[i].append(doc["BLOC-COURBES"]["LESCOURBES"]["C"]["C" + str(i)]["DATAY"]["DonneesY"]["DONNEES"])
 
         table_float = []
 
@@ -50,7 +52,8 @@ def extract(path: str) :
             table_float.append([])
             for j in range(len(table_string[i])):
                 table_float[i].append([])
-                for k in range(int(len(table_string[i][j])/16)):
-                    table_float[i][j].append(struct.unpack('d', binascii.unhexlify(table_string[i][j][k*16:(k+1)*16])))
+                for k in range(int(len(table_string[i][j]) / 16)):
+                    table_float[i][j].append(
+                        struct.unpack('d', binascii.unhexlify(table_string[i][j][k * 16:(k + 1) * 16])))
 
     return table_float
